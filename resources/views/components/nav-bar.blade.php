5d7d6530c66f83aa15447dd8e35b2e3b5dc95211
chinhdtth2009021 <chinhdtth2009021@fpt.edu.vn>
2022-04-06 15:54:49 +0700

    Edit-Home

--- resources/views/components/nav-bar.blade.php
+++ resources/views/components/nav-bar.blade.php
@@ -1,8 +1,8 @@
 <header>
     <div class="top">METAWAY Holdings Roadmap 2022-2027 IPO $300 billion</div>
-    <nav class="navbar navbar-expand-md bg-light">
+    <nav class="navbar navbar-expand-md bg-dark">
         <a class="navbar-brand d-md-none d-block" href="{{ route('home') }}">
-            <img src="{{ asset('img/logo.png') }}" alt="">
+            <img src="{{ asset('img/Asset 2@4x 2.png') }}" alt="">
         </a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <i class="fas fa-bars" aria-hidden="true"></i>
@@ -37,7 +37,7 @@
                 </li>
             </ul>
             <a class="navbar-brand d-none d-md-block" href="{{ route('home') }}">
-                <img src="{{ asset('img/logo.png') }}" alt="">
+                <img src="{{ asset('img/Asset 2@4x 2.png') }}" alt="">
             </a>
             <ul class="navbar-nav">
                 <li class="nav-item dropdown">
