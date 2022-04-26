348b9a0319b7a0d0049f6185fda7c3c651a98d73
hadomanh <hadomanh99@gmail.com>
2022-04-26 17:14:49 +0700

    Add link to news

--- resources/views/media/investor-detail.blade.php
+++ resources/views/media/investor-detail.blade.php
@@ -6,6 +6,8 @@
         <div class="container-lg">
             <div class="row title-investor">
                 <div class="col-lg-8 col-12">
+                    <h3>{!! $investorNews->title !!}</h3>
+                    <br>
                     {!! $investorNews->content !!}
                 </div>
                 <div class="col-lg-4 col-12">
