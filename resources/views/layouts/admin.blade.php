5b47aa98bce2eec1293df614c74fae3858620e21
hadomanh <hadomanh99@gmail.com>
2022-04-26 16:51:03 +0700

    Update title

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -8,7 +8,7 @@
   <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
   <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
   <meta name="viewport" content="width=device-width, initial-scale=1"/>
-  <title>CMCCHAIN Admin</title>
+  <title>METAWAYHOLDINGS Admin</title>
 
   <!-- Google Font: Source Sans Pro -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
