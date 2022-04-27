0c09b0b841b2740233c3ae016c8f36292a7cc750
hadomanh <hadomanh99@gmail.com>
2022-04-27 09:19:18 +0700

    Related news

--- resources/views/media/news-detail.blade.php
+++ resources/views/media/news-detail.blade.php
@@ -1,11 +1,41 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container mt-5">
-        <h3>{{ $news->title }}</h3>
+<div id="mediaNews" class="bg-white">
+    <section class="investor-detail">
+        <div class="container-lg">
+            <div class="row title-investor">
+                <div class="col-lg-8 col-12">
+                    <h3>{!! $news->title !!}</h3>
+                    <br>
+                    {!! $news->content !!}
+                </div>
+                <div class="col-lg-4 col-12">
+                    <div class="title-investor-detail col-12">
+                        <h4><a href="">{{ __('Read more') }}</a></h4>
+                    </div>
 
-        <br>
-
-        {!! $news->content !!}
-    </div>
+                    @foreach ($relatedNews as $item)
+                        <a href="{{ route('media.investor-detail', $item->id) }}">
+                            <div class="d-flex mt-4">
+                                <div class="col-4">
+                                    <img src="{{ $item->thumbnail }}" alt="" class="icon-detail">
+                                </div>
+                                <div class="col-8">
+                                    <h4>{{ $item->title }}</h4>
+                                    <span class="miniSize">{{ date('Y-m-d', strtotime($item->created_at)) }}</span>
+                                </div>
+                            </div>
+                        </a>
+                    @endforeach
+                    
+                </div>
+            </div>
+        </div>
+    </section>
+</div>
 @endsection
+
+@push('script')
+    
+@endpush
