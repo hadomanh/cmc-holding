2949d1a75c2da1cd01de847d9f75a92444e8cf15
hadomanh <hadomanh99@gmail.com>
2022-05-21 09:27:59 +0700

    Update press

--- resources/views/media/press-detail.blade.php
+++ resources/views/media/press-detail.blade.php
@@ -1,11 +1,49 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container mt-5">
-        <h3>{{ $press->title }}</h3>
+<div id="mediaNews" class="bg-white">
+    <section class="investor-detail">
+        <div class="container-lg">
+            <div class="row title-investor">
+                <div class="col-lg-8 col-12 text-left">
+                    <h3>{!! $press->title !!}</h3>
+                    <br>
+                    {!! $press->content !!}
+                </div>
+                <div class="col-lg-4 col-12">
+                    <div class="title-investor-detail col-12">
+                        <h4><a href="">{{ __('Read more') }}</a></h4>
+                    </div>
 
-        <br>
-
-        {!! $press->content !!}
-    </div>
+                    @foreach ($relatedPress as $item)
+                        <a href="{{ route('media.news-detail', $item->id) }}">
+                            <div class="d-flex mt-4">
+                                <div class="col-4">
+                                    <img src="{{ $item->thumbnail }}" alt="" class="icon-detail">
+                                </div>
+                                <div class="col-8">
+                                    <strong style="
+                                        text-align: left;
+                                        overflow: hidden;
+                                        text-overflow: ellipsis;
+                                        display: -webkit-box;
+                                        -webkit-line-clamp: 3;
+                                        line-clamp: 3; 
+                                        -webkit-box-orient: vertical;
+                                    ">{{ $item->title }}</strong>
+                                    <span class="miniSize">{{ date('Y-m-d', strtotime($item->created_at)) }}</span>
+                                </div>
+                            </div>
+                        </a>
+                    @endforeach
+                    
+                </div>
+            </div>
+        </div>
+    </section>
+</div>
 @endsection
+
+@push('script')
+    
+@endpush
